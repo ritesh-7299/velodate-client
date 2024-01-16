16d9ddbb804563181385daba69009ee6066f74c4
ritesh macwan <riteshmacwan07@gmail.com>
2024-01-17 00:26:36 +0530

    added chart setup

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -3,7 +3,45 @@ import React from "react";
 import AdminLayout from "../../layouts/AdminLayout";
 import { Content } from "antd/es/layout/layout";
 import { FiUser } from "react-icons/fi";
+import CanvasJSReact from "@canvasjs/react-charts";
 
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const options = {
+  animationEnabled: true,
+  title: {
+    text: "Monthly Sales - 2017",
+  },
+  axisX: {
+    valueFormatString: "MMM",
+  },
+  axisY: {
+    title: "Sales (in USD)",
+    prefix: "$",
+  },
+  data: [
+    {
+      yValueFormatString: "$#,###",
+      xValueFormatString: "MMMM",
+      type: "spline",
+      dataPoints: [
+        { x: new Date(2017, 0), y: 25060 },
+        { x: new Date(2017, 1), y: 27980 },
+        { x: new Date(2017, 2), y: 42800 },
+        { x: new Date(2017, 3), y: 32400 },
+        { x: new Date(2017, 4), y: 35260 },
+        { x: new Date(2017, 5), y: 33900 },
+        { x: new Date(2017, 6), y: 40000 },
+        { x: new Date(2017, 7), y: 52500 },
+        { x: new Date(2017, 8), y: 32300 },
+        { x: new Date(2017, 9), y: 42000 },
+        { x: new Date(2017, 10), y: 37160 },
+        { x: new Date(2017, 11), y: 38400 },
+      ],
+    },
+  ],
+};
 export default function Dashboard() {
   const {
     token: { colorBgContainer, borderRadiusLG },
@@ -76,21 +114,15 @@ export default function Dashboard() {
             borderRadius: borderRadiusLG,
           }}
         >
-          <p>long content</p>
-          {
-            // indicates very long content
-            Array.from(
-              {
-                length: 100,
-              },
-              (_, index) => (
-                <React.Fragment key={index}>
-                  {index % 20 === 0 && index ? "more" : "..."}
-                  <br />
-                </React.Fragment>
-              )
-            )
-          }
+          <Flex>
+            <div>
+              <CanvasJSChart options={options} />
+            </div>
+            <div>
+              {/* ritesh
+              <CanvasJSChart options={options} /> */}
+            </div>
+          </Flex>
         </div>
       </Content>
     </AdminLayout>
