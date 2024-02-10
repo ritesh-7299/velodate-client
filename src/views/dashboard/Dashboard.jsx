c2eb27c319eb577cbc179089a38be90a7677d4f8
ritesh macwan <riteshmacwan07@gmail.com>
2024-02-10 15:50:18 +0530

    Added: white color to chart titles

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -42,6 +42,7 @@ export default function Dashboard() {
     },
     title: {
       text: "Active users",
+      fontColor: "white",
     },
     axisX: {
       title: "Days",
@@ -65,6 +66,7 @@ export default function Dashboard() {
     },
     title: {
       text: "Visitors",
+      fontColor: "white",
     },
     axisX: {
       title: "Days",
